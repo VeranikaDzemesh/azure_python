4af5c4c4badcbb2d916da8cb2f4a4e1945bf6489
Veranika Dzemesh <v.dzemesh@melsoft-games.com>
2020-11-16 14:42:58 +0300

    no message

--- tests/test_math.py
+++ tests/test_math.py
@@ -10,3 +10,22 @@ def test_two_and_two():
     with assume:
         assert num1 * num2 == 4
 
+
+@allure.description("Check math for three")
+def test_three_and_three():
+    num1 = 3
+    num2 = 3
+    with assume:
+        assert num1 + num2 == 6
+    with assume:
+        assert num1 * num2 == 9
+
+
+@allure.description("Check math for four")
+def test_four_and_four():
+    num1 = 4
+    num2 = 4
+    with assume:
+        assert num1 + num2 == 8
+    with assume:
+        assert num1 * num2 == 16
